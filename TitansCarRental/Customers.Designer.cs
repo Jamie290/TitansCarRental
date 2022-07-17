7070bd285ac74d48eead85f1ba99375398146838
jamie-bah <jamiei-star@hotmail.co.uk>
2022-07-17 18:52:03 +0100

    ADD, DELETE, QUIT buttons coded and working

--- TitansCarRental/Customers.Designer.cs
+++ TitansCarRental/Customers.Designer.cs
@@ -49,9 +49,8 @@ namespace TitansCarRental
             // 
             this.pictureBox1.Image = global::TitansCarRental.Properties.Resources.logo_Titan2;
             this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(228, 131);
+            this.pictureBox1.Size = new System.Drawing.Size(296, 168);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -60,10 +59,9 @@ namespace TitansCarRental
             // 
             this.btnSave.BackColor = System.Drawing.Color.Gray;
             this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.Location = new System.Drawing.Point(32, 410);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Location = new System.Drawing.Point(42, 525);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(115, 36);
+            this.btnSave.Size = new System.Drawing.Size(150, 46);
             this.btnSave.TabIndex = 4;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = false;
@@ -71,10 +69,9 @@ namespace TitansCarRental
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(118, 283);
-            this.txtPhone.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPhone.Location = new System.Drawing.Point(153, 362);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(155, 31);
+            this.txtPhone.Size = new System.Drawing.Size(200, 39);
             this.txtPhone.TabIndex = 5;
             // 
             // label1
@@ -82,20 +79,18 @@ namespace TitansCarRental
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(16, 285);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(21, 365);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 25);
+            this.label1.Size = new System.Drawing.Size(131, 32);
             this.label1.TabIndex = 6;
             this.label1.Text = "Telephone";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(118, 336);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Location = new System.Drawing.Point(153, 430);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(155, 31);
+            this.txtEmail.Size = new System.Drawing.Size(200, 39);
             this.txtEmail.TabIndex = 5;
             // 
             // label2
@@ -103,27 +98,24 @@ namespace TitansCarRental
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(55, 338);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(72, 433);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.Size = new System.Drawing.Size(76, 32);
             this.label2.TabIndex = 6;
             this.label2.Text = "Email";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(118, 173);
-            this.txtID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtID.Location = new System.Drawing.Point(153, 221);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(155, 31);
+            this.txtID.Size = new System.Drawing.Size(200, 39);
             this.txtID.TabIndex = 5;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(118, 227);
-            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Location = new System.Drawing.Point(153, 291);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(155, 31);
+            this.txtName.Size = new System.Drawing.Size(200, 39);
             this.txtName.TabIndex = 5;
             // 
             // label3
@@ -131,10 +123,9 @@ namespace TitansCarRental
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(-3, 173);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(-4, 221);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.Size = new System.Drawing.Size(157, 32);
             this.label3.TabIndex = 6;
             this.label3.Text = "Customer ID";
             // 
@@ -143,10 +134,9 @@ namespace TitansCarRental
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(54, 229);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(70, 293);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 25);
+            this.label4.Size = new System.Drawing.Size(81, 32);
             this.label4.TabIndex = 6;
             this.label4.Text = "Name";
             // 
@@ -154,41 +144,42 @@ namespace TitansCarRental
             // 
             this.btnDelete.BackColor = System.Drawing.Color.Gray;
             this.btnDelete.ForeColor = System.Drawing.Color.White;
-            this.btnDelete.Location = new System.Drawing.Point(152, 410);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Location = new System.Drawing.Point(198, 525);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(115, 36);
+            this.btnDelete.Size = new System.Drawing.Size(150, 46);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnCancel
             // 
             this.btnCancel.BackColor = System.Drawing.Color.Gray;
             this.btnCancel.ForeColor = System.Drawing.Color.White;
-            this.btnCancel.Location = new System.Drawing.Point(272, 410);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Location = new System.Drawing.Point(354, 525);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(115, 36);
+            this.btnCancel.Size = new System.Drawing.Size(150, 46);
             this.btnCancel.TabIndex = 4;
-            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Text = "Quit";
             this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(387, 21);
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(507, 23);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(561, 379);
+            this.listBox1.Size = new System.Drawing.Size(728, 484);
             this.listBox1.TabIndex = 8;
             // 
             // Customers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
-            this.ClientSize = new System.Drawing.Size(960, 585);
+            this.ClientSize = new System.Drawing.Size(1248, 749);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
@@ -202,7 +193,6 @@ namespace TitansCarRental
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Customers";
             this.Text = "Customers";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
